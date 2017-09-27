6bafcc953d7005d8a593c028a48ef76758cd1378
Stephan Druskat <mail@sdruskat.net>
2017-09-27 12:18:12 +0200

    Fix different citation methods Pandoc vs Jekyll

--- build-jekyll.sh
+++ build-jekyll.sh
@@ -10,7 +10,15 @@ fi
 set -e
 
 # Build PDF
-#!/bin/bash
+## Create local copy of index.md
+cp index.md pandoc-index.md
+
+## Replace Liquid-style citations with Pandoc-style citations, keeping page numbers
+ssed -R -i -e 's/ %}/\]/g' pandoc-index.md
+ssed -R -i -e 's/ -l(?= \d)/, p\./g' pandoc-index.md
+ssed -R -i -e 's/{% cite /\[@/g' pandoc-index.md
+
+## Build PDF from tmp file
 pandoc \
 	--latex-engine=xelatex \
 	--toc \
@@ -21,9 +29,13 @@ pandoc \
 	--metadata date="`date '+%d %B %Y'`" \
 	--template=./template/default.latex \
 	-o cff-specifications.pdf \
-	index.md
+	pandoc-index.md
 
+## Move PDF to final destination
 mv ./cff-specifications.pdf ./assets/pdf/cff-specifications.pdf
+
+## Remove tmp file
+rm pandoc-index.md
 
 # cleanup "_site"
 rm -rf _site
--- build.sh
+++ build.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+## Create local copy of index.md
+cp index.md pandoc-index.md
+
+## Replace Liquid-style citations with Pandoc-style citations, keeping page numbers
+ssed -R -i -e 's/ %}/\]/g' pandoc-index.md
+ssed -R -i -e 's/ -l(?= \d)/, p\./g' pandoc-index.md
+ssed -R -i -e 's/{% cite /\[@/g' pandoc-index.md
+
+## Build PDF from tmp file
 pandoc \
+	--latex-engine=xelatex \
 	--toc \
 	--toc-depth=4 \
 	--filter pandoc-citeproc \
-	--bibliography=references.bib \
+	--bibliography=./_bibliography/references.bib \
+	--csl=./_bibliography/ieee-with-url-pandoc.csl \
 	--metadata date="`date '+%d %B %Y'`" \
 	--template=./template/default.latex \
 	-o cff-specifications.pdf \
-	index.md
+	pandoc-index.md
 
+## Move PDF to final destination
 mv ./cff-specifications.pdf ./assets/pdf/cff-specifications.pdf
+
+## Remove tmp file
+rm pandoc-index.md
 exit 0
