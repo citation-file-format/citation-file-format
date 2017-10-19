7b38955295834854f0895bb5e1f96e749622894f
Stephan Druskat <mail@sdruskat.net>
2017-10-19 23:40:00 +0200

    Re-add custom pandoc to build

--- travis-build.sh
+++ travis-build.sh
@@ -3,17 +3,16 @@
 # enable error reporting to the console
 set -e
 
+# Set up and build custom pandoc (for fix of https://github.com/jgm/pandoc/issues/3529)
+git clone https://github.com/jgm/pandoc.git
+cd pandoc
+git checkout 181d737
+stack setup
+stack install --flag pandoc:embed_data_files
+cd ..
+
 # Clone repo
 git clone https://${GITHUB_TOKEN}@github.com/citation-file-format/citation-file-format.github.io.git --branch master _site
-
-
-# Set up and build custom pandoc (for fix of https://github.com/jgm/pandoc/issues/3529)
-# git clone https://github.com/jgm/pandoc.git
-# cd pandoc
-# git checkout 181d737
-# stack setup
-# stack install --flag pandoc:embed_data_files
-# cd ..
 
 # Run the Python script that converts all specifications.md files to PDF
 # pip3 install pypandoc
