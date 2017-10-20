3f90f76af3d6f5ebb5657dbd01a93ad4ac07078d
Stephan Druskat <mail@sdruskat.net>
2017-10-20 09:53:30 +0200

    Update build

--- travis-build.sh
+++ travis-build.sh
@@ -4,30 +4,30 @@
 set -e
 
 # Install Haskell Stack
-curl -sSL https://get.haskellstack.org/ | sh
+# curl -sSL https://get.haskellstack.org/ | sh
 
 # Set up and build custom pandoc (for fix of https://github.com/jgm/pandoc/issues/3529)
-git clone https://github.com/jgm/pandoc.git
-cd pandoc
-git checkout 181d737
-stack setup
-stack install --flag pandoc:embed_data_files
-cd ..
+# git clone https://github.com/jgm/pandoc.git
+# cd pandoc
+# git checkout 181d737
+# stack setup
+# stack install --flag pandoc:embed_data_files
+# cd ..
 
 # Set up and build custom pandoc-citeproc
-git clone https://github.com/jgm/pandoc-citeproc.git
-cd pandoc-citeproc
-git checkout d4bb483
-stack setup
-stack install
-cd ..
+# git clone https://github.com/jgm/pandoc-citeproc.git
+# cd pandoc-citeproc
+# git checkout d4bb483
+# stack setup
+# stack install
+# cd ..
 
 # Clone repo
 git clone https://${GITHUB_TOKEN}@github.com/citation-file-format/citation-file-format.github.io.git --branch master _site
 
 # Run the Python script that converts all specifications.md files to PDF
-echo "Build PDFs"
-python build-pdfs.py
+# echo "Build PDFs"
+# python build-pdfs.py
 
 # Build Jekyll
 echo "Build Jekyll site"
@@ -35,7 +35,7 @@ bundle install
 bundle exec jekyll build
 
 ## push
-echo "Push PDFs"
+echo "Push _site"
 cd _site
 git config user.email "travis-ci@sdruskat.net"
 git config user.name "Travis CI"
