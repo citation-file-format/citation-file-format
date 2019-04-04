c3a076146bdbda647780934347a1aab6c5e89c22
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-04-04 17:03:11 +0200

    moved specifications 1.0.3 from website source files to this repo

--- changes.sh
+++
@@ -1,5 +0,0 @@
-# git log 1.0-RC1.. --pretty=format:'- [%s](http://github.com/citation-file-format/citation-file-format.github.io/commit/%H)' --reverse | grep -v Merge > changes.md
-# git log src... --pretty=format:'- [%s](http://github.com/citation-file-format/citation-file-format.github.io/commit/%H)' --reverse | grep -v Merge > changes.md
-git log --boundary --no-merges --pretty=format:'- [%s](http://github.com/citation-file-format/citation-file-format.github.io/commit/%H)' --reverse src..HEAD | grep -v Merge > 1.X.X-CORRECT-VERSION-HERE-changes.md
-
-# Use in Python script for every dir that contains a specifications.md
