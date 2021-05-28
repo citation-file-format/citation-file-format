16521ac980f89200869a368d79b41d92819ebe2f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-05-28 17:53:38 +0200

    added docstring to explain the pytest fixture magic

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import os
 
 
 def pytest_generate_tests(metafunc):
+    """Generates a list of relative paths to CITATION.cff files under tests/, and
+    uses them to parameterize a fixture by the name of 'fixture'."""
     d = os.path.join(".", "tests")
     fixtures = list()
     for root, dirs, files in os.walk(d):
