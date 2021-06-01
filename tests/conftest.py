4aa9bd667ea4c219124f906d66d2c9e31886c834
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-01 19:00:06 +0200

    fixed a couple of merge errors in the schema; made the 1.2.0 schema testable as part of the github action ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,14 @@ import os
 def pytest_generate_tests(metafunc):
     """Generates a list of relative paths to CITATION.cff files under tests/, and
     uses them to parameterize a fixture by the name of 'fixture'."""
-    d = os.path.join(".", "tests")
+
     fixtures = list()
-    for root, dirs, files in os.walk(d):
-        for name in files:
-            if name == "CITATION.cff":
-                fixtures.append(os.path.join(root, name))
+    schema_versions = ["1.0.3", "1.1.0"]
+    for schema_version in schema_versions:
+        d = os.path.join(".", "tests", schema_version)
+        for root, dirs, files in os.walk(d):
+            for name in files:
+                if name == "CITATION.cff":
+                    fixtures.append(os.path.join(root, name))
 
     metafunc.parametrize("fixture", fixtures)
