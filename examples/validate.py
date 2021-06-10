c902085c6394f6a4e9093290b891615a3b47003f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-10 12:27:31 +0200

    no longer necessary to run validation for 1.2.0 separately

--- examples/validate.py
+++ examples/validate.py
@@ -1,14 +1,19 @@
 import pytest
 from cffconvert import Citation
 from examples.helpers import load_cff
+from examples import schema_poc
 
 
 def test(fixture):
     # (argument fixture is injected from test/conftest.py using pytest fixture magic)
     cffstr = load_cff(fixture)
-    if "fail" in fixture:
-        with pytest.raises(Exception) as e_info:
-            Citation(cffstr=cffstr, suspect_keys=[], validate=True, raise_exception=True)
+    can_test_with_cffconvert = "1.0.3" in fixture or "1.1.0" in fixture
+    if can_test_with_cffconvert:
+        if "fail" in fixture:
+            with pytest.raises(Exception) as e_info:
+                Citation(cffstr=cffstr, suspect_keys=[], validate=True, raise_exception=True)
+        else:
+            citation = Citation(cffstr=cffstr, suspect_keys=[], validate=True, raise_exception=True)
+            assert citation.yaml is not None
     else:
-        citation = Citation(cffstr=cffstr, suspect_keys=[], validate=True, raise_exception=True)
-        assert citation.yaml is not None
+        schema_poc.validate(fixture, 'schema.json')
