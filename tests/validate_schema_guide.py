3c1a5e0ccdd0c529b287614f665dccafed2ca76b
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-08-03 20:08:46 +0200

    superconvoluted test but it seems to do the job

--- tests/validate_schema_guide.py
+++ tests/validate_schema_guide.py
@@ -3,18 +3,13 @@ import os
 import json
 import jsonschema
 from ruamel.yaml import YAML
-from flatten_json import flatten, unflatten_list
 
 
 def test():
 
-    with open("schema-guide.md", "r") as f:
-        markdown = f.read()
-
+    def extract_snippets():
         start = 0
         end = len(markdown)
-        snippets = list()
-
         while start < end:
             snippet_start = markdown.find("```yaml\n", start, end)
             if snippet_start == -1:
@@ -24,59 +19,78 @@ def test():
             indent_size = 0
             while text[8:][indent_size] == " ":
                 indent_size += 1
-            unindented = ""
+            unindented = "\n"
             for line in text[8:-4].split("\n"):
                 unindented += line[indent_size:]
                 unindented += "\n"
-
             snippets.append(dict(start=snippet_start, end=snippet_end + 4, text=unindented))
             start = snippet_end + 4
+        return snippets
+
+    with open("schema-guide.md", "r") as f:
+        markdown = f.read()
+        snippets = list()
+        snippets = extract_snippets()
 
         yaml = YAML(typ='safe')
         yaml.constructor.yaml_constructors[u'tag:yaml.org,2002:timestamp'] = yaml.constructor.yaml_constructors[u'tag:yaml.org,2002:str']
         schema_path = os.path.join(os.path.dirname(__file__), "..", "schema.json")
         with open(schema_path, "r") as sf:
             schema_data = json.loads(sf.read())
-            for snippet in snippets:
+            for i_snippet, snippet in enumerate(snippets):
                 if "# incorrect" in snippet["text"]:
                     continue
                 instance = yaml.load(snippet["text"])
-                # defaults = yaml.load("authors:\n" +
-                #                      "  - name: test\n" +
-                #                      "title: test\n" +
-                #                      "cff-version: 1.2.0\n" +
-                #                      "message: test\n" +
-                #                      "references:\n" +
-                #                      "  - title: test\n" +
-                #                      "    authors:\n" +
-                #                      "      - name: test\n" +
-                #                      "    type: generic\n")
-                defaults = {
-                    "authors": [
-                        {
-                            "family-names": "test"
-                        }
-                    ],
-                    "title": "test",
-                    "cff-version": "1.2.0",
-                    "message": "test",
-                    "references": [
-                        {
-                            "title": "test",
-                            "authors": [
-                                {
-                                    "family-names": "test"
-                                }
-                            ],
-                            "type": "generic"
-                        }
-                    ]
-                }
-                flattened_defaults = flatten(defaults, "/")
-                flattened_instance = flatten(instance, "/")
-                merged = unflatten_list({**flattened_defaults, **flattened_instance}, "/")
-                try:
-                    jsonschema.validate(instance=merged, schema=schema_data, format_checker=jsonschema.FormatChecker())
-                except jsonschema.ValidationError as e:
-                    print("Found a problem with snippet at char position {0}-{1}:\n {2}\n{3}".format(snippet["start"], snippet["end"], snippet["text"], e.message))
-                    raise e
+                passes = False
+                while not passes:
+                    try:
+                        jsonschema.validate(instance=instance, schema=schema_data, format_checker=jsonschema.FormatChecker())
+                        passes = True
+                        print("snippet {0}/{1} (chars {2}-{3}): OK".format(i_snippet + 1, len(snippets), snippet["start"], snippet["end"]))
+                    except jsonschema.ValidationError as e:
+                        path = "" if len(e.relative_path) == 0 else "/".join([str(p) for p in e.relative_path]) + "/"
+                        if path == "":
+                            if e.message.startswith("'authors' is a required property"):
+                                instance["authors"] = []
+                            elif e.message.startswith("'cff-version' is a required property"):
+                                instance["cff-version"] = "1.2.0"
+                            elif e.message.startswith("'message' is a required property"):
+                                instance["message"] = "testmessage"
+                            elif e.message.startswith("'title' is a required property"):
+                                instance["title"] = "testtitle"
+                            else:
+                                raise Exception("undefined behavior")
+                        elif path.startswith("authors"):
+                            if e.message.startswith("[] is too short"):
+                                instance["authors"].append({"name": "testname"})
+                            else:
+                                raise Exception("undefined behavior")
+                        elif path.startswith("references"):
+                            index = int(path.split("/")[1])
+                            if e.message.startswith("'authors' is a required property"):
+                                instance["references"][index]["authors"] = []
+                            elif e.message.startswith("'title' is a required property"):
+                                instance["references"][index]["title"] = "testtitle"
+                            elif e.message.startswith("'type' is a required property"):
+                                instance["references"][index]["type"] = "generic"
+                            elif e.message.startswith("[] is too short"):
+                                instance["references"][index]["authors"].append({"name": "testname"})
+                            elif path.startswith("references/{0}/conference".format(index)):
+                                if e.message.startswith("'name' is a required property"):
+                                    instance["references"][index]["conference"]["name"] = "testname"
+                            else:
+                                raise Exception("undefined behavior")
+                        elif path.startswith("preferred-citation"):
+                            if e.message.startswith("'authors' is a required property"):
+                                instance["preferred-citation"]["authors"] = []
+                            elif e.message.startswith("'title' is a required property"):
+                                instance["preferred-citation"]["title"] = "testtitle"
+                            elif e.message.startswith("'type' is a required property"):
+                                instance["preferred-citation"]["type"] = "generic"
+                            elif e.message.startswith("[] is too short"):
+                                instance["preferred-citation"]["authors"].append({"name": "testname"})
+                            else:
+                                raise Exception("undefined behavior")
+                        else:
+                            print("Found a problem with snippet at char position {0}-{1}:\n {2}\n{3}".format(snippet["start"], snippet["end"], snippet["text"], e.message))
+                            raise e
