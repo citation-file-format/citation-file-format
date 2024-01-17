e3fe17a791e28fa95a4889c8230fc15506fe85ce
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2024-01-17 11:14:03 +0100

    minor tweak to test_cff_files.py

--- tests/test_cff_files.py
+++ tests/test_cff_files.py
@@ -1,12 +1,11 @@
 import jsonschema
-
+import pytest
 from tests.helpers import get_filenames
 from tests.helpers import loadcff
-import pytest
 
 
 @pytest.mark.parametrize("filename", get_filenames())
-def test_cff_files(schema, filename):
+def test(schema, filename):
     def assert_passes():
         try:
             jsonschema.validate(instance=instance, schema=schema, format_checker=jsonschema.FormatChecker())
