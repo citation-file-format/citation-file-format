11aa6005eaa760d0a7d4764b1f213ce45eb66c5f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2024-01-17 11:12:13 +0100

    added helper function to get the project root

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
-from pathlib import Path
 import json
 import pytest
+from tests.helpers import get_project_root
 
 
 @pytest.fixture
 def schema():
-    filename = Path(__file__).parent.parent / "schema.json"
+    filename = get_project_root() / "schema.json"
     with open(filename, "r") as fid:
         txt = fid.read()
     return json.loads(txt)
--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,8 +1,9 @@
 from tests.helpers.get_filenames import get_filenames
+from tests.helpers.get_project_root import get_project_root
 from tests.helpers.loadcff import loadcff
-
 
 __all__ = [
     "get_filenames",
+    "get_project_root",
     "loadcff",
 ]
